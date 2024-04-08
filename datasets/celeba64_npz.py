ad681e24cd0b0e6f70cbfc04a55961c587d1bd67
Mathias Lundteigen Mohus <mathialm@ntnu.no>
2024-04-08 14:19:43 +0200

    Changed dataset resize structure

--- datasets/celeba64_npz.py
+++ datasets/celeba64_npz.py
@@ -1,4 +1,6 @@
 import os
+import pathlib
+
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -7,22 +9,25 @@ import cv2
 
 def imgs_to_npz():
     npz = []
-
     attacks = ["clean",
                "poisoning_simple_replacement-High_Cheekbones-Male",
                "poisoning_simple_replacement-Mouth_Slightly_Open-Wearing_Lipstick"]
+
+
     data_root = "~/poisoning/ML_Poisoning/data/datasets64"
-    dir = os.path.join(data_root, attacks[1], "celeba/")
-    images_dirs = os.listdir(dir)
-    i = 0
-    for img_dir in images_dirs:
-        for img in os.listdir(os.path.join(dir, img_dir)):
-            img_arr = cv2.imread(os.path.join(dir, img_dir, img))
-            img_arr = cv2.cvtColor(img_arr, cv2.COLOR_BGR2RGB)  # cv2默认为 bgr 顺序
-            resized_img = cv2.resize(img_arr, (64, 64))
-            npz.append(resized_img)
-            print(f"Image {i}/202599", end="\r")
-            i += 0
+    folder_folder = os.path.join(data_root, attacks[1], "celeba", "img_align_celeba")
+    folders = [str(f) for f in pathlib.Path(folder_folder).iterdir() if f.is_dir()]
+
+    images = [str(image) for folder in folders for image in pathlib.Path(folder).iterdir() if image.is_file()]
+
+
+    for i, img in enumerate(images):
+        img_arr = cv2.imread(img)
+        img_arr = cv2.cvtColor(img_arr, cv2.COLOR_BGR2RGB)  # cv2默认为 bgr 顺序
+        resized_img = cv2.resize(img_arr, (64, 64))
+        npz.append(resized_img)
+        print(f"Image {i}/202599", end="\r")
+        i += 0
 
     output_npz = np.array(npz)
     np.savez('celeba64_train.npz', output_npz)
